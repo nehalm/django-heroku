7c44bc95fa313e6f22fa3273789302bb3540f979
Nehal <nehal1@gmail.com>
2014-02-02 21:26:18 -0500

    Add base project files

---
+++ project_name/urls.py
@@ -0,0 +1,18 @@
+__author__ = 'file_author'
+
+# Standard Library Imports
+
+# Core Django Imports
+from django.conf.urls import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+# Third Party Imports
+
+# App Imports
+
+urlpatterns = patterns('',
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
