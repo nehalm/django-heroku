6f7b7c2b917ee9c320d80f83a437e2ed8854fd64
Nehal <nehal1@gmail.com>
2014-02-14 14:26:37 -0500

    Added urls, wsgi, views, and placeholder 400/500 pages

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,18 +1,27 @@
+
 __author__ = 'file_author'
 
 # Standard Library Imports
 
 # Core Django Imports
 from django.conf.urls import patterns, include, url
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 # Third Party Imports
 
 # App Imports
+from core.views import IndexTemplateView
+
 
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
+                       # Index
+                       url(
+                           regex=r'^$',
+                           view=IndexTemplateView.as_view(),
+                           name='home'
+                       ),
 )
